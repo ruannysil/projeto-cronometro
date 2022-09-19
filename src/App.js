ddb587ad48de4c44bc727d76dda6a3c292b75815
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2022-09-19 13:18:11 -0300

    projeto cronometro
    projeto criado para testa meu conhecimento em reactjs usando state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            numero: 0,
+            botao: 'COMEÇAR'
+        };
+        this.timer = null;
+        this.comecar = this.comecar.bind(this);
+        this.limpar = this.limpar.bind(this);
+    }
+    comecar() {
+        let state = this.state;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        if(this.timer !== null) {
+            clearInterval(this.timer);
+            this.timer = null;
+            state.botao = 'CONTINUAR'
+        }else{
+            this.timer = setInterval(() =>{
+                let state = this.state;
+                state.numero += 0.1;
+                this.setState(state);
+                
+            },100)
+            state.botao = 'PAUSAR';
+        }
+        this.setState(state);
+    }
+    limpar(){
+       
+        if(this.timer !== null) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+
+        let state = this.state;
+        state.numero = 0;
+        state.botao = 'COMEÇAR';
+        this.setState(state);
+
+    }
+    render(){
+        return(
+            <div className='container'>
+                <img src={require('./assets/cronometro.png')} alt="" className='img' />
+                <span href='' className='timer'>{this.state.numero.toFixed(1)}</span>
+                <div className='areaBtn'>
+                    <span className='botao' onClick={this.comecar}>{this.state.botao}</span>
+                    <span className='botao' onClick={this.limpar}>LIMPAR</span>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
